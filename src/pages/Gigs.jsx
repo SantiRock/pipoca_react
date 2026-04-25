ab0f72c48cb77c396843359c32d5d517685d72d0
SantiRock <sqp.dev@gmail.com>
2026-04-24 19:58:40 -0500

    gigs links

--- src/pages/Gigs.jsx
+++ src/pages/Gigs.jsx
@@ -4,11 +4,11 @@ export const Gigs = () => {
     return(
         <div className="main">
             <div className="player">
-                <a  className="a_gig" href="" target="_blank">
-                    <img className="img_gig" src="./gigs/1may.jpeg" alt="1st may" />
+                <a  className="a_gig" href="https://pipoca.vercel.app/gigs/1may26.jpeg" target="_blank">
+                    <img className="img_gig" src="./gigs/1may26.jpeg" alt="1st may" />
                 </a>
-                <a  className="a_gig" href="" target="_blank">
-                    <img className="img_gig" src="./gigs/1600a.jpeg" alt="Somos Legados" />
+                <a  className="a_gig" href="https://pipoca.vercel.app/gigs/somos_legados.jpeg" target="_blank">
+                    <img className="img_gig" src="./gigs/somos_legados.jpeg" alt="Somos Legados" />
                 </a>
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import "./Pages.css"
 export const Home = ({hometext}) => {
     return(
         <div className="main home_c">
-            <img className="img_home" src="./gigs/1may.jpeg" alt="1st may" />
+            <img className="img_home" src="./gigs/1may26.jpeg" alt="1st may" />
             <p className="ph"></p>
         </div>
     )
